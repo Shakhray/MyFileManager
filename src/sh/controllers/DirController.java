50974efe2534fbf82bda161b1e6a8eb450a5271c
Sherhan <shakhrayas@gmail.com>
2015-12-30 23:48:42 +0300

    refactor DirController

--- src/sh/controllers/DirController.java
+++ src/sh/controllers/DirController.java
@@ -7,7 +7,7 @@ import sh.exceptions.IsNotDirException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -15,39 +15,64 @@ import java.util.regex.Pattern;
  * @author Sherhan
  */
 public class DirController {
+    private final static String DOUBLE_DOT = "..";
+
     public List<String> dir(File directory) {
-        List<String> listFiles = new LinkedList<>();
-        for (File file : directory.listFiles()) {
-            listFiles.add(file.getName());
-        }
-        return listFiles;
+        return Arrays.asList(directory.list());
     }
 
-    public File cd(File homeDir, String goal) throws FileNotFoundException, IsNotDirException, DiskNotFoundException {
-        if (isDiskDriver(goal)) {
-            File disk = new File(goal);
-            if (disk.exists()) {
-                return disk;
-            } else {
-                throw new DiskNotFoundException();
-            }
-        } else if ("..".equals(goal)) {
-            return homeDir.getParentFile() == null ? homeDir : homeDir.getParentFile();
+    public File cd(File homeDir, String targetDir)
+            throws FileNotFoundException, IsNotDirException, DiskNotFoundException {
+        if (isDiskDriver(targetDir)) {
+            return changeDisk(targetDir);
+        } else if (isParentDir(targetDir)) {
+            return parentDir(homeDir);
+        } else {
+            return childDir(homeDir, targetDir);
         }
-        for (File dir : homeDir.listFiles()) {
-            if (goal.equals(dir.getName())) {
-                if (dir.isFile()) {
-                    throw new IsNotDirException();
-                } else {
-                    return dir;
-                }
-            }
-        }
-        throw new FileNotFoundException();
     }
 
     private boolean isDiskDriver(String arguments) {
         return Pattern.matches("^[A-Z]:(\\\\|/)$", arguments);
+    }
+
+    private File changeDisk(String diskName) throws DiskNotFoundException {
+        File disk = new File(diskName);
+        if (disk.exists()) {
+            return disk;
+        } else {
+            throw new DiskNotFoundException();
+        }
+    }
+
+    private boolean isParentDir(String targetDir) {
+        return DOUBLE_DOT.equals(targetDir);
+    }
+
+    private File parentDir(File homeDir) {
+        return homeDir.getParentFile() == null ? homeDir : homeDir.getParentFile();
+    }
+
+    private File childDir(File homeDir, String targetDir) throws IsNotDirException, FileNotFoundException {
+        File childDir = findChild(homeDir, targetDir);
+        if (childDir == null) {
+            throw new FileNotFoundException();
+        } else {
+            return childDir;
+        }
+    }
+
+    private File findChild(File homeDir, String targetDir) throws IsNotDirException {
+        for (File dir : homeDir.listFiles()) {
+            if (targetDir.equals(dir.getName())) {
+                if (dir.isDirectory()) {
+                    return dir;
+                } else {
+                    throw new IsNotDirException();
+                }
+            }
+        }
+        return null;
     }
 
     public void copy(File source, File dest) throws IOException {
